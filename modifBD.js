bedaf567abf983c74e6e91a81352a7344c7ec464
Eloann <eloann76@gmail.com>
2023-11-16 11:34:31 +0100

    ajout formulaire et filtre non fonctionnel

--- modifBD.js
+++ modifBD.js
@@ -36,4 +36,58 @@ function getAlbum(num) {
 getAlbum({ valueOf: function () { return "19"; } });
 
 
+document.getElementById('btnSearch').addEventListener('click', function () {
+  const radioByAlbum = document.getElementById('ChoixTwo');
+  const radioByAuteur = document.getElementById('ChoixOne');
+  const radioBySerie = document.getElementById('ChoixThree');
+  const searchInput = document.getElementById('search-input').value.toLowerCase();
+  const resultsContainer = document.getElementById('results');
 
+  // Efface les résultats précédents
+  resultsContainer.innerHTML = '';
+
+  // Recherche en fonction du critère sélectionné
+     if (searchType === 'Auteur') {
+        console.log("Recherche par auteur");
+        searchByAuteur(searchInput);
+    } else if (searchType === 'Album') {
+        console.log("Recherche par album");
+        searchByAlbum(searchInput);
+    } else if (searchType === 'Serie') {
+        console.log("Recherche par série");
+        searchBySerie(searchInput);
+    } else {
+        console.log("Veuillez sélectionner un critère de recherche.");
+    }
+});
+
+// Fonction pour la recherche par album
+function searchByAlbum(searchInput) {
+  albums.forEach(album => {
+      if (album.titre.toLowerCase().includes(searchInput)) {
+          displayResult(album);
+      }
+  });
+}
+
+// Fonction pour la recherche par auteur
+function searchByAuteur(searchInput) {
+  for (let [idAuteur, auteur] of auteurs.entries()) {
+      for (let [idAlbum, album] of albums.entries()) {
+          if (album.idAuteur == idAuteur && auteur.nom.toLowerCase().includes(searchInput)) {
+              displayResult(album);
+          }
+      }
+  }
+}
+
+// Fonction pour la recherche par série
+function searchBySerie(searchInput) {
+for(let [idSerie, serie] of series.entries()) {
+  for (let [idAlbum, album] of albums.entries()) {
+      if (album.idSerie == idSerie && album.titre.toLowerCase().includes(searchInput)) {
+          console.log(serie.nom+", Album N°"+album.numero+" "+album.titre+", Auteur:"+auteurs.get(album.idAuteur).nom);
+          displayResult(album);
+      }
+  }
+}}
